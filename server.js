ab897a0fe3d1896f7e5ee8e542a8ea4f67f4f131
ZenithSUS <jeranmerino147@gmail.com>
2025-03-03 10:40:16 +0800

    Allow All Origin

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger);
