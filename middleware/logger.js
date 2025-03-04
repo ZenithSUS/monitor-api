ce283ac2d5a42447fed250d8aea3184729028215
ZenithSUS <jeranmerino147@gmail.com>
2025-03-04 11:04:28 +0800

    Enhance logger middleware to include response time and headers, and improve log formatting

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,19 +1,24 @@
 export const logger = (req, res, next) => {
-  console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
-  );
-  console.log(
-    "Time: ",
-    new Date().toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    })
-  );
-
-  if (req.body && Object.keys(req.body).length > 0) {
-    console.log("Body: ", req.body);
-  }
-  console.log(`Middleware status: ${res.statusCode} \n`);
+  const start = Date.now();
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    console.log(
+      `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    );
+    console.log(
+      "Time: ",
+      new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      })
+    );
+    console.log("Headers: ", req.headers);
+    if (req.body && Object.keys(req.body).length > 0) {
+      console.log("Body: ", req.body);
+    }
+    console.log(`Middleware status: ${res.statusCode === 200 ? 'OK' : 'Not Ok'}`);
+    console.log(`Response time: ${duration}ms \n`);
+  });
   next();
 };
